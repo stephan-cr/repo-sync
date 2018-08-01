68681dc5aff0bbb72b8a26d201a2db34f36780a4
stephan_cr <stephan_cr@localhost>
2018-08-01 10:52:51 +0200

    start repo-sync implementation

---
+++ README.rst
@@ -0,0 +1,4 @@
+Repo-sync
+=========
+
+Keep repositories in sync with upstream.
---
+++ sync-repos.py
@@ -0,0 +1,36 @@
+import json
+import os
+import subprocess
+import sys
+
+supported_repo_types = ['hg', 'git', 'svn']
+
+def execute_cmd(*args):
+    return subprocess.call(args, stdout=sys.stdout, stderr=sys.stderr)
+
+def main():
+    config_dir = os.path.dirname(sys.argv[0])
+    conf = None
+    with open(os.path.join(config_dir, 'sync-repos.conf'), 'r') as conf_file:
+        try:
+            conf = json.load(conf_file)
+        except ValueError as json_exception:
+            print >> sys.stderr, json_exception
+            sys.exit(1)
+
+    for item in conf:
+        repo_type = item['repo-type']
+        if repo_type in supported_repo_types:
+            os.chdir(item['dir'])
+            print "%s >>> %s" % (repo_type, item['dir'])
+            if repo_type == 'hg':
+                execute_cmd('hg', 'pull', '-u')
+            elif repo_type == 'git':
+                execute_cmd('git', 'pull')
+            elif repo_type == 'svn':
+                execute_cmd('svn', 'update')
+            else:
+                print "unknown"
+
+if __name__ == '__main__':
+    main()
