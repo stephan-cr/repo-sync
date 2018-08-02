6b04cc3a656890baaed744968fac7ca13bf9b60a
stephan_cr <stephan_cr@localhost>
2018-08-02 06:35:37 +0200

    port to python 3

--- sync-repos.py
+++ sync-repos.py
@@ -15,14 +15,14 @@ def main():
         try:
             conf = json.load(conf_file)
         except ValueError as json_exception:
-            print >> sys.stderr, json_exception
+            print(json_exception, file=sys.stderr)
             sys.exit(1)
 
     for item in conf:
         repo_type = item['repo-type']
         if repo_type in supported_repo_types:
             os.chdir(item['dir'])
-            print "%s >>> %s" % (repo_type, item['dir'])
+            print("%s >>> %s" % (repo_type, item['dir']))
             if repo_type == 'hg':
                 execute_cmd('hg', 'pull', '-u')
             elif repo_type == 'git':
@@ -30,7 +30,7 @@ def main():
             elif repo_type == 'svn':
                 execute_cmd('svn', 'update')
             else:
-                print "unknown"
+                print('unknown')
 
 if __name__ == '__main__':
     main()
